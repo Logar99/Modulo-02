cefcb0ed9a30fb592d86353912edf5fb80d1296e
Logar99 <108686330+Logar99@users.noreply.github.com>
2023-02-01 20:36:07 -0600

    Add files via upload

---
+++ sesión03/modelo/Saludo.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Saludo {
+    private String mensaje;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+
+}
+
