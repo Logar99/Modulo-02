3925135d51a940c1e2a31614606331025e9af27e
Logar99 <108686330+Logar99@users.noreply.github.com>
2023-02-04 21:37:13 -0600

    Add files via upload
    Log4j

---
+++ sesión07/Class07Log4JApplication.java
@@ -0,0 +1,13 @@
+package com.example.class07log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class07Log4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Class07Log4JApplication.class, args);
+    }
+
+}
