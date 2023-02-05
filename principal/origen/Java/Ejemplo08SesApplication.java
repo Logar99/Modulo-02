83b68cce61df3103427b7294531f42117472378b
Logar99 <108686330+Logar99@users.noreply.github.com>
2023-02-04 22:59:17 -0600

    Add files via upload

---
+++ principal/origen/Java/Ejemplo08SesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo08ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo08SesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ejemplo08SesApplication.class, args);
+    }
+
+}
---
+++ principal/origen/Java/Ejemplo08SesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo08ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo08SesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
